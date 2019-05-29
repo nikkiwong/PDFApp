1ccc3057856f00b93886191ed6173b0166fa101d
Nikki Wong <Nikki.Wong@version1.com>
2019-05-29 09:16:39 +0100

    added formatting

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -5,9 +5,9 @@ import GroupDragDrop from './components/groupDragDrop/GroupDragDrop'
 import DisplayResults from './components/displayResults/DisplayResults'
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       splitSuccessful: false,
       groupingSuccessful: false,
       data: null,
@@ -15,24 +15,24 @@ class App extends Component {
     }
     this.isSplitSuccessful = this.isSplitSuccessful.bind(this);
     this.isGroupingSuccessful = this.isGroupingSuccessful.bind(this);
+  }
+
+  isSplitSuccessful(splitSuccessful, data) {
+    const PDFFileNames = []
+    for (let i = 0; i < data["data"].length; i++) {
+      PDFFileNames.push({ id: i + 1, pdfName: data["data"][i], type: "PDFHolder", backgroundColor: "red" })
     }
+    this.setState({ splitSuccessful: splitSuccessful, data: PDFFileNames });
+  }
 
-    isSplitSuccessful(splitSuccessful, data){
-     const PDFFileNames = []
-       for ( let i = 0; i<data["data"].length; i++){
-         PDFFileNames.push({id: i+1, pdfName:data["data"][i],type:"PDFHolder", backgroundColor: "red"})
-       }
-       this.setState({ splitSuccessful: splitSuccessful, data: PDFFileNames });
-   }
-
-   isGroupingSuccessful(groupingSuccessful, groupingData){
-     this.setState({ groupingSuccessful: groupingSuccessful, groupingData:groupingData });
-   }
+  isGroupingSuccessful(groupingSuccessful, groupingData) {
+    this.setState({ groupingSuccessful: groupingSuccessful, groupingData: groupingData });
+  }
   render() {
     return (
       <div className="App">
-        { (this.state.splitSuccessful) ?
-          (this.state.groupingSuccessful ? <DisplayResults results={this.state.groupingData}/> : <GroupDragDrop onGroupSuccessful={this.isGroupingSuccessful} pdfData={this.state.data}/>) : <Upload onSplitPDF={this.isSplitSuccessful}/>
+        {(this.state.splitSuccessful) ?
+          (this.state.groupingSuccessful ? <DisplayResults results={this.state.groupingData} /> : <GroupDragDrop onGroupSuccessful={this.isGroupingSuccessful} pdfData={this.state.data} />) : <Upload onSplitPDF={this.isSplitSuccessful} />
         }
       </div>
     );
--- react-frontend/src/components/upload/Upload.js
+++ react-frontend/src/components/upload/Upload.js
@@ -1,29 +1,29 @@
 
 import React, { Component } from 'react';
-import {post} from 'axios';
+import { post } from 'axios';
 import Split from '../split/Split'
 class Upload extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      file:''
+    this.state = {
+      file: ''
     }
   }
 
-  onChange(e){
-    let files=e.target.files;
+  onChange(e) {
+    let files = e.target.files;
     let reader = new FileReader();
     reader.readAsDataURL(files[0]);
 
-    reader.onload=(e)=>{
-      const url="http://127.0.0.1:5000/api/upload";
-      const formData={file:e.target.result}
-      return post(url, formData).then(response=> console.log("result", response))
+    reader.onload = (e) => {
+      const url = "http://127.0.0.1:5000/api/upload";
+      const formData = { file: e.target.result }
+      return post(url, formData).then(response => console.log("result", response))
     }
   }
 
-  splitPDF(e){
+  splitPDF(e) {
     var headers = new Headers();
     headers.append('Accept', 'application/json');
 
@@ -31,10 +31,11 @@ class Upload extends Component {
     headers.append('Access-Control-Allow-Credentials', 'true');
 
     // headers.append('GET', 'POST', 'OPTIONS');
-    var requestOptions = { method: 'GET',
-                   headers: headers,
-                   cache: 'default',
-                  };
+    var requestOptions = {
+      method: 'GET',
+      headers: headers,
+      cache: 'default',
+    };
 
     e.preventDefault();
     fetch('http://127.0.0.1:5000/api/split', requestOptions)
@@ -43,18 +44,18 @@ class Upload extends Component {
         this.setState({ file: data })
         this.props.onSplitPDF(true, data)
       })
-    .catch((error)=>{
-      console.log(error)
-    })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
   render() {
     return (
       <div className="App">
         <h1>Upload your pdf</h1>
-        <input type="file" name="file" onChange={(e)=>this.onChange(e)}></input>
+        <input type="file" name="file" onChange={(e) => this.onChange(e)}></input>
         <div>
-          <input type="submit" name="split" value="Split PDF" onClick={(e)=> this.splitPDF(e)}></input>
+          <input type="submit" name="split" value="Split PDF" onClick={(e) => this.splitPDF(e)}></input>
         </div>
       </div>
     );
@@ -62,158 +63,3 @@ class Upload extends Component {
 }
 
 export default Upload;
-
-
-
-// import React, { Component } from "react";
-// import Dropzone from "../dropzone/Dropzone";
-// import "./Upload.css";
-// import Progress from "../progress/Progress";
-
-// class Upload extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       files: [],
-//       uploading: false,
-//       uploadProgress: {},
-//       successfullUploaded: false
-//     };
-
-//     this.onFilesAdded = this.onFilesAdded.bind(this);
-//     this.uploadFiles = this.uploadFiles.bind(this);
-//     this.sendRequest = this.sendRequest.bind(this);
-//     this.renderActions = this.renderActions.bind(this);
-//   }
-
-//   onFilesAdded(files) {
-//     this.setState(prevState => ({
-//       files: prevState.files.concat(files)
-//     }));
-//   }
-
-//   async uploadFiles() {
-//     this.setState({ uploadProgress: {}, uploading: true });
-//     const promises = [];
-//     this.state.files.forEach(file => {
-//       promises.push(this.sendRequest(file));
-//     });
-//     try {
-//       await Promise.all(promises);
-
-//       this.setState({ successfullUploaded: true, uploading: false });
-//     } catch (e) {
-//       // Not Production ready! Do some error handling here instead...
-//       this.setState({ successfullUploaded: true, uploading: false });
-//     }
-//   }
-
-//   sendRequest(file) {
-//     return new Promise((resolve, reject) => {
-//       const req = new XMLHttpRequest();
-
-//       req.upload.addEventListener("progress", event => {
-//         if (event.lengthComputable) {
-//           const copy = { ...this.state.uploadProgress };
-//           copy[file.name] = {
-//             state: "pending",
-//             percentage: (event.loaded / event.total) * 100
-//           };
-//           this.setState({ uploadProgress: copy });
-//         }
-//       });
-
-//       req.upload.addEventListener("load", event => {
-//         const copy = { ...this.state.uploadProgress };
-//         copy[file.name] = { state: "done", percentage: 100 };
-//         this.setState({ uploadProgress: copy });
-//         resolve(req.response);
-//       });
-
-//       req.upload.addEventListener("error", event => {
-//         const copy = { ...this.state.uploadProgress };
-//         copy[file.name] = { state: "error", percentage: 0 };
-//         this.setState({ uploadProgress: copy });
-//         reject(req.response);
-//       });
-
-//       const formData = new FormData();
-//       formData.append("file", file, file.name);
-
-//       req.open("POST", "http://127.0.0.1:5000/upload");
-//       req.send(formData);
-//     });
-//   }
-
-//   renderProgress(file) {
-//     const uploadProgress = this.state.uploadProgress[file.name];
-//     if (this.state.uploading || this.state.successfullUploaded) {
-//       return (
-//         <div className="ProgressWrapper">
-//           <Progress progress={uploadProgress ? uploadProgress.percentage : 0} />
-//           <img
-//             className="CheckIcon"
-//             alt="done"
-//             src="baseline-check_circle_outline-24px.svg"
-//             style={{
-//               opacity:
-//                 uploadProgress && uploadProgress.state === "done" ? 0.5 : 0
-//             }}
-//           />
-//         </div>
-//       );
-//     }
-//   }
-
-//   renderActions() {
-//     if (this.state.successfullUploaded) {
-//       return (
-//         <button
-//           onClick={() =>
-//             this.setState({ files: [], successfullUploaded: false })
-//           }
-//         >
-//           Clear
-//         </button>
-//       );
-//     } else {
-//       return (
-//         <button
-//           disabled={this.state.files.length < 0 || this.state.uploading}
-//           onClick={this.uploadFiles}
-//         >
-//           Upload
-//         </button>
-//       );
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div className="Upload">
-//         <span className="Title">Upload Files</span>
-//         <div className="Content">
-//           <div>
-//             <Dropzone
-//               onFilesAdded={this.onFilesAdded}
-//               disabled={this.state.uploading || this.state.successfullUploaded}
-//             />
-//           </div>
-//           <div className="Files">
-//             {this.state.files.map(file => {
-//               return (
-//                 <div key={file.name} className="Row">
-//                   <span className="Filename">{file.name}</span>
-//                   {this.renderProgress(file)}
-//                 </div>
-//               );
-//             })}
-//           </div>
-//         </div>
-//         <div className="Actions">{this.renderActions()}</div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Upload;
